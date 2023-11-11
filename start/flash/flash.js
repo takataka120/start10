2eab65ecad364bdc1ef58e164825377a78ff73aa
taka <sadaharu12345@outlook.jp>
2023-11-11 09:42:19 +0900

    flash暗算作ります

--- start/flash/flash.js
+++ start/flash/flash.js
@@ -1,21 +1,23 @@
-
-
-var display = document.querySelector('#display');
-var number = document.querySelector('#number');
-var start = document.querySelector('#start');
-var answer = document.querySelector('#answer');
-var total, count, prev, timer;
+const display = document.querySelector('#display');
+const number = document.querySelector('#number');
+const start = document.querySelector('#start');
+const answer = document.querySelector('#answer');
+let total, count, prev, timer;
+let speed = 500;
 
 start.onclick = function () {
   number.value = '';
   total = count = prev = 0;
+  clearInterval(timer);
+
   timer = setInterval(function () {
-    var i = Math.ceil(Math.random() * 99);
-    if (prev == i) {
+    const i = Math.ceil(Math.random() * 99);
+    if (prev === i) {
       i++;
     }
     prev = i;
-    if (count == 5) {
+
+    if (count === 5) {
       clearInterval(timer);
       display.innerHTML = '';
     } else {
@@ -23,8 +25,8 @@ start.onclick = function () {
       display.innerHTML = i;
     }
     count++;
-  }, 500)
-}
+  }, 500 / speed);
+};
 
 answer.onclick = function () {
   if (number.value == total) {
@@ -32,4 +34,28 @@ answer.onclick = function () {
   } else {
     display.innerHTML = total;
   }
-}
+};
+
+// スピード調整用の入力フィールドを追加
+const speedInput = document.querySelector('#speed');
+
+speedInput.addEventListener('input', function () {
+  speed = parseInt(speedInput.value);
+  clearInterval(timer);
+  timer = setInterval(function () {
+    const i = Math.ceil(Math.random() * 99);
+    if (prev === i) {
+      i++;
+    }
+    prev = i;
+
+    if (count === 5) {
+      clearInterval(timer);
+      display.innerHTML = '';
+    } else {
+      total += i;
+      display.innerHTML = i;
+    }
+    count++;
+  }, 500 / speed);
+});
