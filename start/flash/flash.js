673440cc8f5686e55fa64c3f923f29488364229c
taka <sadaharu12345@outlook.jp>
2023-12-13 11:07:12 +0900

    フラッシュ暗算アプリをブラッシュアップ

--- start/flash/flash.js
+++ start/flash/flash.js
@@ -2,47 +2,66 @@ const display = document.querySelector('#display');
 const number = document.querySelector('#number');
 const start = document.querySelector('#start');
 const answer = document.querySelector('#answer');
+const reset = document.querySelector('#reset');
+const speedInput = document.querySelector('#speed');
+
 let total, count, prev, timer;
 let speed = 8;
 //display, number, start, answerはHTML要素を参照しています。これらはそれぞれ、数値を表示する場所、ユーザーが答えを入力する場所、ゲームを開始するボタン、答えを提出するボタンを指しています。
 start.onclick = function () {
-  number.value = '';//number.value = ''; は、指定されたHTML要素である number というIDを持つ要素（おそらく <input> 要素）の値を空文字列に設定しています。このコードは、ユーザーが <input> 要素に入力した値をクリアし、空の状態にリセットするために使用されます。一般的な使用ケースとして、ゲームやアプリケーションなどで、新しいゲームやタスクを開始する前に入力フィールドをリセットする必要がある場合に、このようなコードが使用されます。入力フィールドを空にすることで、ユーザーは新しい入力を開始できます
+  document.getElementById('instructions').innerHTML = '';
+  number.value = '';
   total = count = prev = 0;//total = count = prev = 0; は、3つの変数 total、count、および prev をすべて0に初期化するコードです。この行のコードは、これらの3つの変数を同時に初期化しています。
   clearInterval(timer);//clearInterval(timer) は、指定されたタイマーをクリア（停止）するためのJavaScriptのメソッドです。この行のコードは、変数 timer に保存されているタイマーを停止します。
 
   timer = setInterval(function () {
-    let i = Math.ceil(Math.random() * 99);
+    let i = Math.ceil(Math.random() * 10);//ここは桁数　10を掛けてからMath.ceilを使って切り上げています。この結果、1から10の整数が得られます。
     if (prev === i) {
-      i++;
+      i++;//同じ値になってもいいけど避けておく
     }
     prev = i;
 
     if (count === 5) {
       clearInterval(timer);//clearInterval(timer): ゲームが終了した場合、clearInterval 関数が呼び出されて、タイマーを停止します。これにより、ゲームのタイマーが停止し、新しいゲームの開始が妨げられます。
-      display.innerHTML = '';//display.innerHTML = '';: ゲームの終了時に、display というIDを持つ要素（おそらくHTML内のテキスト表示用の要素）の innerHTML プロパティを空に設定して、表示領域をクリアします。
+      display.innerHTML = '';//
     } else {
       total += i;
       display.innerHTML = i;
     }
     count++;
-  }, speed * 1000);
+  }, 1000 / speed);
 };
 //answer.onclick関数は、答えを提出するボタンがクリックされたときに実行されます。この関数は、ユーザーが入力した答えが合計と一致するかどうかを確認し、結果を表示します。
 answer.onclick = function () {
   if (number.value == total) {
     display.innerHTML = '正解！';
   } else {
-    display.innerHTML = 'ざんねん!! 合計は ' + total + ' でした。';
+    display.innerHTML = 'おしい!! 合計は ' + total + ' でした。';
   }
 };
 
 // スピード調整用の入力フィールドを追加
-const speedInput = document.querySelector('#speed');
 
 speedInput.addEventListener('input', function () {
-  speed = parseFloat(speedInput.value); // parseIntをparseFloatに変更
+  let inputValue = parseFloat(speedInput.value);
+  if (inputValue <= 0) {
+    alert('スピードは0より大きい値を入力してください。');
+    speedInput.value = 1; // デフォルトの値を設定
+    speed = 1;
+  } else {
+    speed = inputValue;
+  }
 });
 
+
+reset.onclick = function () {
+  // RESETボタンがクリックされたときに実行する処理
+  clearInterval(timer); // タイマーを停止
+  display.innerHTML = ''; // 表示をクリア
+  number.value = ''; // 入力フィールドをクリア
+  total = count = prev = 0; // 変数をリセット
+  document.getElementById('instructions').innerHTML = 'スピードを調整してstartを押してください';
+};
 
 
 /*
